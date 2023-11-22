8d717cfd8e2cc584135741b36100141f992e7574
Agata <agata.thrams@gmail.com>
2023-11-22 15:02:57 +0100

    homescreen functions

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,58 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+
+const getCategoriesFromData = (data: any) => {
+  let temp: any = {};
+  for (let i = 0; i < data.length; i++) {
+    if (temp[data[i].name] == undefined) {
+      temp[data[i].name] = 1;
+    } else {
+      temp[data[i].name]++;
+    }
+  }
+  let categories = Object.keys(temp);
+  categories.unshift('All');
+  return categories;
+};
+
+const getCoffeeList = (category: string, data: any) => {
+  if (category == 'All') {
+    return data;
+  } else {
+    let coffeeList = data.filter(
+      (item: any) => InteractionManager.name == category,
+    );
+    return coffeeList;
+  }
+};
+
+const HomeScreen = () => {
+  const CoffeeList = useStore((state: any) => state.CoffeeList);
+  const BeansList = useStore((state: any) => state.BeansList);
+
+  const [categories, setCategories] = useState(
+    getCategoriesFromData(CoffeeList),
+  );
+  const [searchText, setSearchText] = useState(undefined);
+  const [categoryIndex, setCategoryIndex] = useState({
+    index: 0,
+    category: categories[0],
+  });
+  const [sortedCoffee, setSortedCoffee] = useState(
+    getCoffeeList(categoryIndex.category, CoffeeList),
+  );
+
+  const tabBarHeight = useBottomTabBarHeight();
+
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
